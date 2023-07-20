038133981a288515d71133004f160af65138bb9f
Joshua <7747985+yeenbean@users.noreply.github.com>
2023-07-20 13:30:58 -0700

    Add Prettier code formatter and disable format-based linting
    - Added Prettier code formatter and related packages.
    - Modified .eslintrc.cjs to include the prettier plugin, disabling format-based linting by typescript-eslint.
    - Prettier is now responsible for code formatting.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,17 @@
 module.exports = {
     extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended'
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'prettier'
     ],
     parser: '@typescript-eslint/parser',
     plugins: [
         '@typescript-eslint'
     ],
+    parserOptions: {
+        project: true,
+        tsconfigRootDir: __dirname
+    },
     root: true
 }
